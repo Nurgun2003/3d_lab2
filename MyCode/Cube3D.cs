e674ee722a3ea85fdee27046b84f364ae15cbec9
Nurgun2003 <90663118+Nurgun2003@users.noreply.github.com>
2025-04-18 14:23:14 +0900

    Добавьте файлы проекта.

---
+++ MyCode/Cube3D.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Windows;//добавлен для использования структуры Point
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+using System.Windows.Documents;
+
+//====================================================
+// Описание работы классов и методов исходника на:
+// https://www.interestprograms.ru
+// Исходные коды программ и игр
+//====================================================
+
+namespace lab2.MyCode
+{
+    public class Cube3D : ModelVisual3D
+    {
+        public Cube3D()
+        {
+            DrawCube(_size, _pos);
+        }
+
+        private double _size = 0.5;
+        public double Size
+        {
+            get => _size;
+            set
+            {
+                _size = value;
+
+                DrawCube(_size, _pos);
+            }
+        }
+
+        private Point3D _pos;
+        public Point3D Position
+        {
+            get => _pos;
+            set
+            {
+                _pos = value;
+
+                DrawCube(_size, _pos);
+            }
+        }
+
+        // Материалы граней
+        public ImageBrush _front = new ImageBrush(new BitmapImage(new Uri("r1.jpg", UriKind.Relative)));
+        public ImageBrush _top = new ImageBrush(new BitmapImage(new Uri("r2.jpg", UriKind.Relative)));
+        public ImageBrush _left = new ImageBrush(new BitmapImage(new Uri("r3.jpg", UriKind.Relative)));
+        public ImageBrush _right = new ImageBrush(new BitmapImage(new Uri("r4.jpg", UriKind.Relative)));
+        public ImageBrush _back = new ImageBrush(new BitmapImage(new Uri("r5.jpg", UriKind.Relative)));
+        public ImageBrush _bottom = new ImageBrush(new BitmapImage(new Uri("r6.jpg", UriKind.Relative)));
+
+        private static GeometryModel3D AddFace(
+            Point3D point1,
+            Point3D point2,
+            Point3D point3,
+            Point3D point4,
+            Material material)
+        {
+            GeometryModel3D geometryModel3D = new GeometryModel3D
+            {
+                Geometry = new MeshGeometry3D()
+                {
+                    Positions = new Point3DCollection
+                    {
+                        point1,
+                        point2,
+                        point3,
+                        point3,
+                        point4,
+                        point1
+                    },
+                    TextureCoordinates = new PointCollection
+                    {
+                        new Point(0, 1),
+                        new Point(0, 0),
+                        new Point(1, 0),
+                        new Point(1, 0),
+                        new Point(1, 1),
+                        new Point(0, 1)
+                    }
+                }
+            };
+            geometryModel3D.Material = material;
+            return geometryModel3D;
+        }
+
+
+        private void DrawCube(
+            double size,
+            Point3D pos
+            )
+        {
+            // Отсчёт точек от левого нижнего угла грани.
+
+
+            // Размерности граней симметричны в обе стороны в абсолютных величинах.
+            double absX = size / 2;
+            double absY = size / 2;
+            double absZ = size / 2;
+
+
+            Point3D front_left_bottom = new Point3D(-absX + pos.X, -absY + pos.Y, absZ + pos.Z);
+            Point3D front_right_bottom = new Point3D(absX + pos.X, -absY + pos.Y, absZ + pos.Z);
+            Point3D front_right_top = new Point3D(absX + pos.X, absY + pos.Y, absZ + pos.Z);
+            Point3D front_left_top = new Point3D(-absX + pos.X, absY + pos.Y, absZ + pos.Z);
+            Point3D backside_right_top = new Point3D(absX + pos.X, absY + pos.Y, -absZ + pos.Z);
+            Point3D backside_left_top = new Point3D(-absX + pos.X, absY + pos.Y, -absZ + pos.Z);
+            Point3D backside_left_bottom = new Point3D(-absX + pos.X, -absY + pos.Y, -absZ + pos.Z);
+            Point3D backside_right_bottom = new Point3D(absX + pos.X, -absY + pos.Y, -absZ + pos.Z);
+
+
+            Model3DGroup m3dg = new Model3DGroup();
+
+            // 1 Передняя
+            DiffuseMaterial material = new DiffuseMaterial();
+            material.Brush = _front;
+            GeometryModel3D faceFront = AddFace(
+                    front_left_bottom,
+                    front_right_bottom,
+                    front_right_top,
+                    front_left_top,
+                    material);
+
+            m3dg.Children.Add(faceFront);
+
+            // 2 Верхняя
+
+            material = new DiffuseMaterial();
+            material.Brush = _top;
+            GeometryModel3D faceTop =
+                AddFace(
+                    front_left_top,
+                    front_right_top,
+                    backside_right_top,
+                    backside_left_top,
+                    material);
+            m3dg.Children.Add(faceTop);
+
+            // 3 Левая
+            material = new DiffuseMaterial();
+            material.Brush = _left;
+            GeometryModel3D faceLeft =
+                AddFace(
+                    backside_left_bottom,
+                    front_left_bottom,
+                    front_left_top,
+                    backside_left_top,
+                    material);
+            m3dg.Children.Add(faceLeft);
+
+            // 4 Правая
+            material = new DiffuseMaterial();
+            material.Brush = _right;
+            GeometryModel3D faceRight =
+                AddFace(
+                    front_right_bottom,
+                    backside_right_bottom,
+                    backside_right_top,
+                    front_right_top,
+                    material);
+            m3dg.Children.Add(faceRight);
+
+            // 5 Нижняя
+            material = new DiffuseMaterial();
+            material.Brush = _bottom;
+            GeometryModel3D faceBottom =
+                AddFace(
+                    backside_left_bottom,
+                    backside_right_bottom,
+                    front_right_bottom,
+                    front_left_bottom,
+                    material);
+            m3dg.Children.Add(faceBottom);
+
+            // 6 Задняя
+            material = new DiffuseMaterial();
+            material.Brush = _back;
+            GeometryModel3D faceBack =
+                AddFace(
+                    backside_right_bottom,
+                    backside_left_bottom,
+                    backside_left_top,
+                    backside_right_top,
+                    material);
+            m3dg.Children.Add(faceBack);
+            Content = m3dg;
+        }
+    }
+}
---
+++ MyCode/Sphere3D.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Media.Media3D;
+
+namespace lab2.MyCode
+{
+    public class Sphere3D : ModelVisual3D
+    {
+        private const int max_i = 60; //на сколько полос по долготе (longitudes)
+        private const int max_j = 60; //на сколько полос по широте (latitudes)
+        private Point3D[,] position = new Point3D[max_i + 1, max_j]; //массив для точек
+        private Point[,] texture = new Point[max_i + 1, max_j];
+
+        private void GenerateSphere(int longitudes, int latitudes)
+        {
+            double latitudeArcusIncrement = Math.PI / (latitudes - 1);
+            double longitudeArcusIncrement = 2.0 * Math.PI / longitudes;
+            for (int lat = 0; lat < latitudes; lat++)
+            {
+                double latitudeArcus = lat * latitudeArcusIncrement; //угол по вертикали
+                double radius = Math.Sin(latitudeArcus); //радиус цилиндра
+                double y = Math.Cos(latitudeArcus);
+                double textureY = (double)lat / (latitudes - 1);
+                for (int lon = 0; lon <= longitudes; lon++)
+                {
+                    double longitudeArcus = lon * longitudeArcusIncrement;
+                    position[lon, lat].X = radius * Math.Cos(longitudeArcus);
+                    position[lon, lat].Y = y;
+                    position[lon, lat].Z = -radius * Math.Sin(longitudeArcus);
+                    texture[lon, lat].X = (double)lon / longitudes;
+                    texture[lon, lat].Y = textureY;
+                }
+            }
+        }
+
+        public BitmapImage moonImage = new BitmapImage(new Uri("moon.bmp", UriKind.Relative));
+        //массив для материалов, т.е. кисти для текстуры
+        private DiffuseMaterial[] frontMaterial = new DiffuseMaterial[max_j - 1];
+
+        private void GenerateImageMaterials()
+        {
+            ImageBrush imageBrush;
+            double flatThickness = 1.0 / (max_i - 1); // ширина полосы
+            double minus = (double)(max_i);//???
+            for (int i = 0; i < max_i - 1; i++)
+            {
+                imageBrush = new ImageBrush((BitmapImage)moonImage);// для каждой полосы готовим кисть
+                                                                     //задаем активную область текстуры для кисти. 
+                                                                     //Это относится ко всей полосе цилиндра
+                imageBrush.Viewbox = new Rect(0, i * flatThickness, minus / max_i, flatThickness);
+                frontMaterial[i] = new DiffuseMaterial(imageBrush);
+            }
+        }
+
+        private void GenerateAllCylinders()
+        {
+            //Создаем объект Model3DGroup для всей сферы
+            Model3DGroup model3DGroup = new Model3DGroup();
+            for (int lat = 0; lat < max_j - 1; lat++)//цикл по всем полосам
+            {
+                //Создаем объект GeometryModel3D для полосы
+                GeometryModel3D geometryModel3D = new GeometryModel3D();
+                geometryModel3D.Geometry = GenerateCylinder(lat); //заполнение треугольников полосы
+                geometryModel3D.Material = frontMaterial[lat];//назначение текстурной кисти
+                model3DGroup.Children.Add(geometryModel3D);
+            }
+            //Созданный объект model3DGroup является геометрическим представлением сферы
+            Content = model3DGroup;
+        }
+
+        private MeshGeometry3D GenerateCylinder(int lat)
+        {
+            MeshGeometry3D meshGeometry3D = new MeshGeometry3D();
+            for (int lon = 0; lon <= max_i; lon++)       //для каждой точки параллели
+            {
+                Point3D p0 = position[lon, lat];           //точка на параллели
+                Point3D p1 = position[lon, lat + 1];       //точка на параллели выше
+                meshGeometry3D.Positions.Add(p0);
+                meshGeometry3D.Positions.Add(p1);
+                meshGeometry3D.Normals.Add((Vector3D)p0);  //Задаем нормали
+                meshGeometry3D.Normals.Add((Vector3D)p1);  //
+                meshGeometry3D.TextureCoordinates.Add(texture[lon, lat]); // координаты текстуры
+                meshGeometry3D.TextureCoordinates.Add(texture[lon, lat + 1]); // координаты текстуры
+            }
+            for (int lon = 1; lon < meshGeometry3D.Positions.Count - 2; lon += 2)
+            { //первый треугольник = левая верхняя часть четырехугольника
+                meshGeometry3D.TriangleIndices.Add(lon - 1); //точка левая верхняя
+                meshGeometry3D.TriangleIndices.Add(lon); //точка левая нижняя
+                meshGeometry3D.TriangleIndices.Add(lon + 1); //точка правая верняя
+                                                             //второй треугольник – нижняя правая часть четурехугольника
+                meshGeometry3D.TriangleIndices.Add(lon + 1); //точка справа выше
+                meshGeometry3D.TriangleIndices.Add(lon); //левая нижняя
+                meshGeometry3D.TriangleIndices.Add(lon + 2); //правая нижняя точка
+            }
+            return meshGeometry3D; //возвращаем полосу
+        }
+
+        public Sphere3D()
+        {
+            GenerateImageMaterials();
+            GenerateSphere(max_i, max_j);
+            GenerateAllCylinders();
+        }
+    }
+}
